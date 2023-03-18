11e1f5fd7a238b96843a828b53c6aa315b4a2aa9
asung <a.city521@gmail.com>
2023-03-18 16:38:39 +0900

    추가: styled-components 테마 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Home from "pages/Home";
+import {ThemeProvider} from "styled-components";
 import GlobalStyles from "styles/GlobalStyles";
+import {theme} from "styles/theme";
 
 function App() {
   return (
-      <div>
+      <ThemeProvider theme={theme}>
         <GlobalStyles />
-          <Home />
-      </div>
+          <div>
+            <Home />
+          </div>
+      </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+const colors = {
+  beige: "#E3C290",
+  yellow: "#F8C353",
+  lightBrown: "#996633",
+  brown: "#644024",
+  darkBrown: "#120804",
+};
+
+export const theme = {
+  colors,
+};
