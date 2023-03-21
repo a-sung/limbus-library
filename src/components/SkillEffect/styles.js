b3146eebe108c9930b71998259889639f4f47601
asung <a.city521@gmail.com>
2023-03-21 23:08:49 +0900

    수정: 스킬 호버 박스 위치 수정

--- src/components/SkillEffect/styles.js
+++ src/components/SkillEffect/styles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   position: absolute;
-  top: 95%;
-  left: 16px;
+  top: 100%;
+  left: 0;
   width: 366px;
   z-index: 3;
   border: 1px solid #644024;
