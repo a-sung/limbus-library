1ce8e24e88fce88c0990817a723c8fac5ad0cfe6
asung <a.city521@gmail.com>
2023-03-21 18:44:50 +0900

    추가: 캐릭터 스킬 별 코인 수 표시

--- src/components/Skill/styles.js
+++ src/components/Skill/styles.js
@@ -5,7 +5,8 @@ export const SkillContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `;
-export const SkillIcon = styled.div`
+
+export const SkillFrame = styled.div`
   width: ${(props) => (props.width ? props.width : "53px")};
   height: ${(props) => (props.height ? props.height : "50px")};
   background-color: ${(props) => (props.color ? props.color : "#120804")};
@@ -15,11 +16,13 @@ export const SkillIcon = styled.div`
   align-items: center;
   color: ${({ theme }) => theme.colors.beige};
 `;
+
 export const SkillPowerText = styled.div`
   font-family: 'S-CoreDream-3Light', sans-serif;
   font-size: 1.5rem;
   font-weight: bold;
 `;
+
 export const CoinPowerText = styled.div`
   background-color: ${(props) => (props.color ? props.color : "gray")};
   text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;
@@ -28,10 +31,21 @@ export const CoinPowerText = styled.div`
   margin-top: 4px;
 `;
 
+export const NameText = styled.div`
+  margin: 6px 0;
+  padding: 2px 8px;
+  background-color: ${(props) => (props.color ? props.color : "black")};
+  box-shadow: -2px 3px black;
+  font-family: 'S-CoreDream-3Light', sans-serif;
+  font-weight: bold;
+  text-shadow: 1px 1px black;
+`;
+
 export const IconsWrap = styled.div`
   display: flex;
   gap: 2px;
 `;
+
 export const Icon = styled.div`
   width: 20px;
   display: inline-block;
@@ -42,12 +56,13 @@ export const Icon = styled.div`
   }
 `;
 
-export const SkillNameText = styled.div`
-  margin: 4px 0;
-  padding: 2px 8px;
-  background-color: ${(props) => (props.color ? props.color : "black")};
-  box-shadow: -2px 3px black;
-  font-family: 'S-CoreDream-3Light', sans-serif;
-  font-weight: bold;
-  text-shadow: 1px 1px black;
+export const CoinsWrap = styled(IconsWrap)`
+  gap: 0;
+`;
+
+export const CoinIcon = styled(Icon)`
+  width: 14px;
+  > img {
+    height: 12px;
+  }
 `;
--- src/data/characters.js
+++ src/data/characters.js
@@ -23,10 +23,10 @@ export async function getCharacters() {
         status:{health, speed, defense},
         resistances: {slash, pierce, blunt},
         skills: [
-          {name: fir_name, type: fir_type, affinity: fir_aff, power: fir_pow},
-          {name: sec_name, type: sec_type, affinity: sec_aff, power: sec_pow},
-          {name: thi_name, type: thi_type, affinity: thi_aff, power: thi_pow},
-          {name: def_type, type: def_type, affinity: def_aff, power: def_pow}
+          {name: fir_name, type: fir_type, affinity: fir_aff, power: {count: fir_pow.split('/')[0], skill: fir_pow.split('/')[1], coin: fir_pow.split('/')[2]}},
+          {name: sec_name, type: sec_type, affinity: sec_aff, power: {count: sec_pow.split('/')[0], skill: sec_pow.split('/')[1], coin: sec_pow.split('/')[2]}},
+          {name: thi_name, type: thi_type, affinity: thi_aff, power: {count: thi_pow.split('/')[0], skill: thi_pow.split('/')[1], coin: thi_pow.split('/')[2]}},
+          {name: def_type, type: def_type, affinity: def_aff, power: {count: def_pow.split('/')[0], skill: def_pow.split('/')[1], coin: def_pow.split('/')[2]}}
         ],
         defense: {name: def_type, type: def_type, affinity: def_aff, power: def_pow},
       }));
