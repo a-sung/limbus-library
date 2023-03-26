178737422003aeae24e5984768b352d6d52f47bb
asung <a.city521@gmail.com>
2023-03-26 18:44:01 +0900

    추가: i18n에 영어 추가

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,22 +2,23 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import ko from "./locales/ko.json";
 import jp from "./locales/jp.json";
+import en from "./locales/en.json";
 
 const resources = {
   ko: {translation: ko,},
   jp: {translation: jp,},
+  en: {translation: en,},
 };
 
 i18n
     .use(initReactI18next)
     .init({
       resources,
-      lng: "ko",
-      fallbackLng: 'ko',
+      lng: "en",
+      fallbackLng: 'en',
       debug: true, // TODO: 배포 시 삭제
       defaultNS: "translation",
       ns: "translation",
-      // keySeparator: false,
       interpolation: {
         escapeValue: false,
       },
