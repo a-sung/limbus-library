9fcad02a325325729038c567beee1f369b86e1fb
asung <a.city521@gmail.com>
2023-03-31 22:21:45 +0900

    추가: 선택된 언어 강조

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,17 +8,18 @@ function Header() {
     i18n.changeLanguage(language)
     localStorage.setItem("language", i18n.language);
   }
-
   return (
       <S.Container>
         <header>
           <S.Title>Limbus Company 도감</S.Title>
         </header>
-        <div>
-          <button onClick={() => changeLanguage('ko')}>KO/</button>
-          <button onClick={() => changeLanguage('jp')}>JP/</button>
-          <button onClick={() => changeLanguage('en')}>EN</button>
-        </div>
+        <S.LanguageWrap>
+          <S.LanguageButton onClick={() => changeLanguage('ko')} active={i18n.language === 'ko' ? 'active' : null}>KO</S.LanguageButton>
+          <span> / </span>
+          <S.LanguageButton onClick={() => changeLanguage('jp')} active={i18n.language === 'jp' ? 'active' : null}>JP</S.LanguageButton>
+          <span> / </span>
+          <S.LanguageButton onClick={() => changeLanguage('en')} active={i18n.language === 'en' ? 'active' : null}>EN</S.LanguageButton>
+        </S.LanguageWrap>
 
       </S.Container>
   );
