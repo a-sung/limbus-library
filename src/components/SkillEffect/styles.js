6a11d18c4b5bd6718f008e93ccdf21e4e916f886
asung <a.city521@gmail.com>
2023-04-01 23:31:34 +0900

    수정: 스킬 상세 박스 텍스트 정렬 수정

--- src/components/SkillEffect/styles.js
+++ src/components/SkillEffect/styles.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 export const Container = styled.div`
   position: absolute;
   top: 100%;
-  left: 0;
+  left: -1px;
   width: 366px;
   z-index: 3;
   border: 1px solid #644024;
   text-align: start;
-  padding: 6px;
+  padding: 8px;
   background-color: #120804;
   white-space: pre-wrap;
   line-height: 1.2rem;
@@ -18,4 +18,15 @@ export const SKillNameText = styled.div`
   //font-family: 'S-CoreDream-3Light', 'M PLUS Rounded 1c', sans-serif;
   font-size: 1.2em;
   font-weight: bold;
+  margin-bottom: 4px;
+`;
+
+export const SkillText = styled.div`
+  display: flex;
+  gap: 4px;
+  font-family: sans-serif;
+  > div:last-child {
+    flex-basis: 0;
+    flex-grow: 1;
+  }
 `;
